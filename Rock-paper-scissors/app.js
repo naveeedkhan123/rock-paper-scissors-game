3347d12788ccf851b757680a4aab339b71152835
Naveed khane <naveeedkhan1237@gmail.com>
2026-06-26 02:59:54 -0700

    created A simple Rock Paper Scissors game built using HTML, CSS and JavaScript.

---
+++ Rock-paper-scissors/app.js
@@ -0,0 +1,61 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const msg = document.querySelector("#msg");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+  // rock , paper , scissors
+};
+
+const drawGame = () => {
+  msg.innerText = "Game was Draw. Play Again !";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const showWinnwer = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `you won ! your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = userScore;
+    msg.innerText = `you won !  ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userChoice) => {
+  // Generate computer choice
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinnwer(userWin, userChoice, compChoice);
+  }
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
